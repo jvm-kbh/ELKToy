569f63f5cf7038f70a3a5fe630df5b29ad4c4034
jvm-kbh <elasticsearch@kakao.com>
2023-10-31 11:13:06 +0900

    first commit

---
+++ src/main/java/me/kbh/elktoy/ElkToyApplication.java
@@ -0,0 +1,13 @@
+package me.kbh.elktoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkToyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElkToyApplication.class, args);
+  }
+
+}
