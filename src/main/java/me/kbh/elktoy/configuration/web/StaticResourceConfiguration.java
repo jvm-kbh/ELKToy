927b387275e0cf20bbc13614051931469ff48f88
jvm-kbh <elasticsearch@kakao.com>
2023-11-10 03:47:55 +0900

    - html design - javascript module pattern    - page, result event - static resource handler configuration

---
+++ src/main/java/me/kbh/elktoy/configuration/web/StaticResourceConfiguration.java
@@ -0,0 +1,13 @@
+package me.kbh.elktoy.configuration.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
--- src/main/resources/static/result-event.js
+++ src/main/resources/static/result-event.js
@@ -1,40 +1,55 @@
 var resultEvent = (function () {
-  var _generate = function(data){
-    var template = $('#commercial-property-template').html();
+  var _generate = function (dataList) {
+    $.each(dataList, function (index, item) {
+      var template = $('#commercial-property-template').html();
 
-    template = template.split("{%name%}").join("data.name");
-    template = template.split("{%branchName%}").join("data.branchName");
-    template = template.split("{%majorCategoryName%}").join("data.majorCategoryName");
-    template = template.split("{%middleCategoryName%}").join("data.middleCategoryName");
-    template = template.split("{%subCategoryName%}").join("data.subCategoryName");
-    template = template.split("{%industryName%}").join("data.industryName");
-    template = template.split("{%cityName%}").join("data.cityName");
-    template = template.split("{%districtName%}").join("data.districtName");
-    template = template.split("{%administrativeDistrictName%}").join("data.administrativeDistrictName");
-    template = template.split("{%legalDistrictName%}").join("data.legalDistrictName");
-    template = template.split("{%landCategoryName%}").join("data.landCategoryName");
-    template = template.split("{%parcelCode%}").join("data.parcelCode");
-    template = template.split("{%parcelAddress%}").join("data.parcelAddress");
-    template = template.split("{%parcelMainNumber%}").join("data.parcelMainNumber");
-    template = template.split("{%parcelSubNumber%}").join("data.parcelSubNumber");
-    template = template.split("{%roadCode%}").join("data.roadCode");
-    template = template.split("{%roadName%}").join("data.roadName");
-    template = template.split("{%roadAddress%}").join("data.roadAddress");
-    template = template.split("{%buildingMainNumber%}").join("data.buildingMainNumber");
-    template = template.split("{%buildingSubNumber%}").join("data.buildingSubNumber");
-    template = template.split("{%buildingManagementNumber%}").join("data.buildingManagementNumber");
-    template = template.split("{%buildingName%}").join("data.buildingName");
-    template = template.split("{%oldZipCode%}").join("data.oldZipCode");
-    template = template.split("{%newZipCode%}").join("data.newZipCode");
-    template = template.split("{%dongInfo%}").join("data.dongInfo");
-    template = template.split("{%floorInfo%}").join("data.floorInfo");
-    template = template.split("{%unitInfo%}").join("data.unitInfo");
-    template = template.split("{%longitude%}").join("data.longitude");
-    template = template.split("{%latitude%}").join("data.latitude");
+      template = template.split("{%name%}").join(item.name);
+      template = template.split("{%branchName%}").join(item.branchName);
+      template = template.split("{%majorCategoryName%}").join(
+          item.majorCategoryName);
+      template = template.split("{%middleCategoryName%}").join(
+          item.middleCategoryName);
+      template = template.split("{%subCategoryName%}").join(
+          item.subCategoryName);
+      template = template.split("{%industryName%}").join(item.industryName);
+      template = template.split("{%cityName%}").join(item.cityName);
+      template = template.split("{%districtName%}").join(item.districtName);
+      template = template.split("{%administrativeDistrictName%}").join(
+          item.administrativeDistrictName);
+      template = template.split("{%legalDistrictName%}").join(
+          item.legalDistrictName);
+      template = template.split("{%landCategoryName%}").join(
+          item.landCategoryName);
+      template = template.split("{%parcelCode%}").join(item.parcelCode);
+      template = template.split("{%parcelAddress%}").join(item.parcelAddress);
+      template = template.split("{%parcelMainNumber%}").join(
+          item.parcelMainNumber);
+      template = template.split("{%parcelSubNumber%}").join(
+          item.parcelSubNumber);
+      template = template.split("{%roadCode%}").join(item.roadCode);
+      template = template.split("{%roadAddress%}").join(item.roadAddress);
+      template = template.split("{%buildingMainNumber%}").join(
+          item.buildingMainNumber);
+      template = template.split("{%buildingSubNumber%}").join(
+          item.buildingSubNumber);
+      template = template.split("{%buildingManagementNumber%}").join(
+          item.buildingManagementNumber);
+      template = template.split("{%buildingName%}").join(item.buildingName);
+      template = template.split("{%oldZipCode%}").join(item.oldZipCode);
+      template = template.split("{%newZipCode%}").join(item.newZipCode);
+      template = template.split("{%dongInfo%}").join(item.dongInfo);
+      template = template.split("{%floorInfo%}").join(item.floorInfo);
+      template = template.split("{%unitInfo%}").join(item.unitInfo);
+      template = template.split("{%longitude%}").join(item.longitude);
+      template = template.split("{%latitude%}").join(item.latitude);
 
-    $("#data-component").append(template);
+      $("#data-component").append(template);
+    });
+  }, _clearPage = function () {
+    $("#data-component").empty();
   }
   return {
-    generate : _generate
+    clearPage: _clearPage,
+    generate: _generate
   }
 })();
