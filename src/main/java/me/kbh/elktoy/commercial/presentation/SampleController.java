7c4a9a1699ad8bacfab7919a6d43defe5badd1e2
jvm-kbh <elasticsearch@kakao.com>
2024-01-12 13:55:15 +0900

    - move package

--- src/main/java/me/kbh/elktoy/commercial/presentation/SampleController.java
+++ src/main/java/me/kbh/elktoy/commercial/presentation/SampleController.java
@@ -1,4 +1,4 @@
-package me.kbh.elktoy.doc.presentation;
+package me.kbh.elktoy.commercial.presentation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
