32b118d449a18500f04a7d19a502062b21fb638a
jvm-kbh <elasticsearch@kakao.com>
2024-01-12 13:55:02 +0900

    - reformat code

--- src/main/java/me/kbh/elktoy/configuration/web/StaticResourceConfiguration.java
+++ src/main/java/me/kbh/elktoy/configuration/web/StaticResourceConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticResourceConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    }
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+  }
 }
